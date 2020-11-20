68651bff3f55512c1f3abd7e7dc3010285742696
Douglas Nhunzvi <dougiedj@gmail.com>
2020-11-20 11:48:43 +0200

    Commit by Douglas Nhunzvi

--- app/src/main/java/com/example/dairbordzimbabwe/activities/Splash.java
+++ app/src/main/java/com/example/dairbordzimbabwe/activities/Splash.java
@@ -2,7 +2,12 @@ package com.example.dairbordzimbabwe.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -12,12 +17,32 @@ public class Splash extends AppCompatActivity {
 
     private TextView tvTitle;
     private ImageView imgLogo;
+    private Animation bottom, top;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN );
+
         tvTitle = findViewById( R.id.tvTitle );
         imgLogo = findViewById( R.id.imgLogo );
+
+        bottom = AnimationUtils.loadAnimation( this, R.anim.bottom );
+        top = AnimationUtils.loadAnimation( this, R.anim.top );
+
+        tvTitle.setAnimation( bottom );
+        imgLogo.setAnimation( top );
+
+        int SPLASH_SCREEN = 5000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent( Splash.this, MainActivity.class );
+                startActivity( intent );
+                finish();
+            }
+        }, SPLASH_SCREEN);
     }
 }
