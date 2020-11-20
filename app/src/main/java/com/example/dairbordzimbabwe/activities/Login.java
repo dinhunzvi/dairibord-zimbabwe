1afda820a04637b1c353da2c723fd71628501659
Douglas Nhunzvi <dougiedj@gmail.com>
2020-11-20 12:18:07 +0200

    Commit by Douglas Nhunzvi

--- app/src/main/java/com/example/dairbordzimbabwe/activities/Login.java
+++ app/src/main/java/com/example/dairbordzimbabwe/activities/Login.java
@@ -6,12 +6,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.dairbordzimbabwe.R;
+import com.google.android.material.textfield.TextInputEditText;
 
 public class Login extends AppCompatActivity {
+
+    private TextInputEditText edtUsername, edtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        edtPassword = findViewById( R.id.edtPassword );
+        edtUsername = findViewById( R.id.edtUsername );
+
     }
 }
