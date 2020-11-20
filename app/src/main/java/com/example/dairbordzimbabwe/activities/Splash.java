d4b06da85f299a4a19f3a80c57b7d5111ab81338
Douglas Nhunzvi <dougiedj@gmail.com>
2020-11-20 11:42:27 +0200

    Commit by Douglas Nhunzvi

--- app/src/main/java/com/example/dairbordzimbabwe/activities/Splash.java
+++ app/src/main/java/com/example/dairbordzimbabwe/activities/Splash.java
@@ -3,14 +3,21 @@ package com.example.dairbordzimbabwe.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.dairbordzimbabwe.R;
 
 public class Splash extends AppCompatActivity {
 
+    private TextView tvTitle;
+    private ImageView imgLogo;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        tvTitle = findViewById( R.id.tvTitle );
+        imgLogo = findViewById( R.id.imgLogo );
     }
 }
